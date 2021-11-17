9c999199c5c7c6a6b1de260a83f8b8097ccee784
Xun Chen <chenxun@sjtu.edu.cn>
2021-11-17 17:01:40 +0800

    try to read the indexed file

--- ppbs.cc
+++ ppbs.cc
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <memory>
 #include <string>
@@ -9,11 +10,6 @@
 
 namespace py = pybind11;
 namespace pf = pbsf;
-
-auto group_data_reader(const std::string &ifname) {
-    auto data_file = open_sequential_input_file(ifname, PandaXIVRealm());
-    return data_file.read_one_type<GroupData>();
-}
 
 struct GroupReader {
     GroupReader() = delete;
@@ -41,11 +37,69 @@ struct GroupReader {
     }
 };
 
+struct GroupIndexedReader {
+    GroupIndexedReader() = delete;
+    GroupIndexedReader(const std::string &ifname)
+        : data_file{std::make_shared<decltype(
+              pf::open_indexed_input_file<EventIndex>(ifname, PandaXIVRealm()))>(
+              std::move(pf::open_indexed_input_file<EventIndex>(
+                  ifname, PandaXIVRealm())))},
+          is{data_file->begin()} {}
+    std::shared_ptr<decltype(pbsf::open_indexed_input_file<EventIndex>(std::declval<std::string>(), PandaXIVRealm()))> data_file;
+    decltype(data_file->begin()) is;
+    bool begin = true;
+    std::pair<EventIndex, GroupData> nextData() {
+	if (begin) {
+            if (is == data_file->end()) {
+                return {{}, {}};
+            }
+            begin = false;
+	    auto idx = is->first;
+	    auto gd = is->second->template as <GroupData>();
+            return {idx, gd};
+        }
+	++is;
+        if (is != data_file->end()) {
+            return {is->first, is->second->template as<GroupData>()};;
+        } else {
+            return {{}, {}};
+        }
+    }
+};
+
+struct PEIndexedReader {
+    PEIndexedReader() = delete;
+    PEIndexedReader(const std::string &s)
+        : data_file{std::make_shared<decltype(
+              pf::open_indexed_input_file<EventIndex>(s, PandaXIVRealm()))>(
+              std::move(pf::open_indexed_input_file<EventIndex>(
+                  s, PandaXIVRealm())))},
+          is{data_file->begin()} {}
+    std::shared_ptr<decltype(pbsf::open_indexed_input_file<EventIndex>(std::declval<std::string>(), PandaXIVRealm()))> data_file;
+    decltype(data_file->begin()) is;
+    bool begin = true;
+    std::pair<EventIndex, PhysicalEventData> nextData() {
+	if (begin) {
+            if (is == data_file->end()) {
+                return {{}, {}};
+            }
+            begin = false;
+	    auto idx = is->first;
+	    auto gd = is->second->template as <PhysicalEventData>();
+            return {idx, gd};
+        }
+	++is;
+        if (is != data_file->end()) {
+            return {is->first, is->second->template as<PhysicalEventData>()};;
+        } else {
+            return {{}, {}};
+        }
+    }
+};
+
 PYBIND11_MODULE(ppbs, m) {
     m.doc() = "pandax-4t pbs data reader plugin";
     using namespace pybind11::literals;
-    m.def("group_data_reader", &group_data_reader,
-          "a function to read group data file", "file"_a);
 
     py::class_<GroupReader>(m, "GroupReader")
         .def(py::init<const std::string &>())
@@ -57,4 +111,34 @@ PYBIND11_MODULE(ppbs, m) {
 	.def_readonly("groupNumber", &GroupData::groupNumber)
 	.def_readonly("startTime", &GroupData::startTime)
 	.def_readonly("endTime", &GroupData::endTime);
+
+    py::class_<EventIndex>(m, "EventIndex")
+	.def(py::init<>())
+	.def_readonly("runNumber", &EventIndex::runNumber)
+	.def_readonly("fileNumber", &EventIndex::fileNumber)
+	.def_readonly("eventNumber", &EventIndex::eventNumber);
+
+    py::class_<GroupIndexedReader>(m, "GroupIndexedReader")
+        .def(py::init<const std::string &>())
+        .def("getNext", &GroupIndexedReader::nextData);
+
+    py::class_<EventNumber>(m, "EventNumber")
+	.def(py::init<>())
+	.def_readonly("eventNumber", &EventNumber::eventNumber);
+
+    py::class_<PhysicalEventData>(m, "PhysicalEventData")
+	.def(py::init<>())
+	.def_readonly("runNumber", &PhysicalEventData::runNumber)
+	.def_readonly("number", &PhysicalEventData::number);
+
+    py::enum_<HitType>(m, "PhysicalEventType")
+	.value("NORMAL", HitType::NORMAL)
+	.value("NOISE", HitType::NOISE)
+	.value("SATURATE", HitType::SATURATE)
+	.value("BASELINE", HitType::BASELINE)
+	.export_values();
+
+    py::class_<PEIndexedReader>(m, "PEIndexedReader")
+	.def(py::init<const std::string &>())
+	.def("getNext", &PEIndexedReader::nextData);
 }
